6207afff8536982c23df5f5b9894130bbe1f352c
popovpp <popovpp@yandexru>
2020-12-12 16:39:17 +0300

    App connected to MySQL, correct error on the main page.

--- labwork006/settings.py
+++ labwork006/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'labwork006.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',                  
+        'USER': 'lw006',             
+        'PASSWORD': 'lw006@121220',                  
+        'HOST': '',                     
+        'PORT': '',
     }
 }
 
